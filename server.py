cd8e4bc30c7603f90bd20cd6a601c51e72b201f0
std.67122420214 <std.67122420214@ubru.ac.th>
2025-12-09 09:48:57 +0700

    First commit

---
+++ server.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return f'<h1>Hello World?</h1>'
+
+@app.route('/name')
+def name():
+  return f'<h1>nontagorn</h1>'
+if  __name__=='__main__':
+  app.run(debug=True)
